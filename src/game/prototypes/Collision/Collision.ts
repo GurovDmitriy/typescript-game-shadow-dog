eaf2205e05a21cab92e512e595c21dc01ecc139b
Gurov Dmitriy <gurovdmitriy1991@gmail.com>
2023-03-12 16:44:17 +0300

    feat: base for collision

---
+++ src/game/prototypes/Collision/Collision.ts
@@ -0,0 +1,49 @@
+class Collision {
+  public square(rect1: Rect, rect2: Rect) {
+    if (
+      rect1.x > rect2.x + rect2.width ||
+      rect1.x + rect1.width < rect2.x ||
+      rect1.y > rect2.y + rect2.height ||
+      rect1.y + rect1.height < rect2.y
+    ) {
+      console.log("no collision")
+      return false
+    } else {
+      console.log("collision")
+      return true
+    }
+  }
+
+  public circle(circle1: Circle, circle2: Circle) {
+    const dx = circle2.x - circle1.x
+    const dy = circle2.y - circle1.y
+
+    const distance = Math.sqrt(dx * dx + dy * dy)
+    const sumOfRadius = circle1.radius + circle2.radius
+
+    if (distance < sumOfRadius) {
+      console.log("collision")
+      return true
+    } else if (distance === sumOfRadius) {
+      console.log("touching")
+    } else {
+      console.log("no collision")
+      return false
+    }
+  }
+}
+
+type Rect = {
+  x: number
+  y: number
+  width: number
+  height: number
+}
+
+type Circle = {
+  x: number
+  y: number
+  radius: number
+}
+
+export { Collision }
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-import "./game/index.ts"
+// import "./game/index.ts"
+
+console.log("collision")
+
+const rect1 = { x: 5, y: 5, width: 50, height: 50 }
+const rect2 = { x: 5, y: 5, width: 50, height: 10 }
+
+if (
+  rect1.x > rect2.x + rect2.width ||
+  rect1.x + rect1.width < rect2.x ||
+  rect1.y > rect2.y + rect2.height ||
+  rect1.y + rect1.height < rect2.y
+) {
+  console.log("no collision")
+} else {
+  console.log("collision")
+}
 
 export {}
