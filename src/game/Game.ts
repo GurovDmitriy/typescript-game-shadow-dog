41d93386724c0015aa32f3a72a0ca85caf2ac7fc
Gurov Dmitriy <gurovdmitriy1991@gmail.com>
2023-07-01 19:11:24 +0300

    refactor: run and timer for game part

--- src/game/Game.ts
+++ src/game/Game.ts
@@ -1,7 +1,6 @@
 import { BTN, TYPE_ACTION } from "../engine/Controller"
 import { IController } from "../engine/Engine"
 import Background from "./models/Background/Background"
-import ShadowDog from "./models/ShadowDog/ShadowDog"
 
 /**
  * Game
@@ -11,105 +10,65 @@ class Game implements IGame {
   private _controller: IController
 
   private _background: Background
-  private _dog: ShadowDog
-
-  private _isMoveBg = false
-  private _timerIdMoveBg = null
-
-  private _isRunDog = false
-  private _isSitDog = false
-  private _timerIdRunDog = null
+  private _runBg: () => void
 
   constructor(ctx: CanvasRenderingContext2D, controller: IController) {
     this._ctx = ctx
     this._controller = controller
 
-    this._background = new Background(this._ctx)
-    this._dog = new ShadowDog(this._ctx, 6)
-
-    /**
-     * Binding callbacks for Background
-     */
-    this._controller.define(
-      TYPE_ACTION.keydown,
-      BTN.arrowRight,
-      this._setMoveBg.bind(this)
-    )
-
-    /**
-     * Binding callbacks for Dog
-     */
-    this._controller.define(
-      TYPE_ACTION.keydown,
-      BTN.arrowRight,
-      this._setRunDog.bind(this)
-    )
-    this._controller.define(
-      TYPE_ACTION.keydown,
-      BTN.arrowDown,
-      this._setSitDog.bind(this)
-    )
+    this._background = new Background(ctx)
+    this._runBg = this._setBg()
   }
 
   /**
    * Run game cycle
    */
   public run() {
-    this._moveBg()
-    this._runDog()
+    this._runBg()
   }
 
   /**
-   * Callbacks for btn
+   * Settings for Background
    */
-  private _setMoveBg() {
-    clearTimeout(this._timerIdMoveBg)
+  private _setBg() {
+    let active = false
 
-    this._isMoveBg = true
+    const setActive = this._timeoutAction("bg", () => {
+      active = true
+      return () => (active = false)
+    })
 
-    this._timerIdMoveBg = setTimeout(() => {
-      this._isMoveBg = false
-    }, 100)
-  }
+    this._controller.define(
+      TYPE_ACTION.keydown,
+      BTN.arrowRight,
+      setActive.bind(this)
+    )
 
-  private _setRunDog() {
-    clearTimeout(this._timerIdRunDog)
-
-    this._isRunDog = true
-
-    this._timerIdRunDog = setTimeout(() => {
-      this._isRunDog = false
-    }, 100)
-  }
-
-  private _setSitDog() {
-    clearTimeout(this._timerIdRunDog)
-
-    this._isSitDog = true
-
-    this._timerIdRunDog = setTimeout(() => {
-      this._isSitDog = false
-    }, 100)
-  }
-
-  /**
-   * Actions
-   */
-  private _moveBg() {
-    if (this._isMoveBg) {
-      this._background.animate().updateSpeed(2)
-    } else {
-      this._background.animate().updateSpeed(0)
+    return function run() {
+      if (active) {
+        this._background.animate().updateSpeed(2)
+      } else {
+        this._background.animate().updateSpeed(0)
+      }
     }
   }
 
-  private _runDog() {
-    if (this._isRunDog) {
-      this._dog.run()
-    } else if (this._isSitDog) {
-      this._dog.sit()
-    } else {
-      this._dog.plain()
+  private _timeoutAction(timerName, cb: () => () => void) {
+    const timers = {}
+
+    return function () {
+      clearTimeout(timers[timerName])
+      const alternate = cb()
+
+      if (timers[timerName] === undefined) {
+        timers[timerName] = null
+        return
+      }
+
+      timers[timerName] = setTimeout(() => {
+        timers[timerName] = undefined
+        alternate()
+      }, 80)
     }
   }
 }
