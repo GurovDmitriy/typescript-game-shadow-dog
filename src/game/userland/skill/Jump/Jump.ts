d30c15d508e95aed7a239a1fa0b7339713bfc133
GurovDmitriy <gurovdmitriy1991@gmail.com>
2023-10-06 13:14:55 +0300

    refactor: skill jump distance

--- src/game/userland/skill/Jump/Jump.ts
+++ src/game/userland/skill/Jump/Jump.ts
@@ -2,24 +2,47 @@ import { Skill } from "../Skill"
 import { ICreatorCharacter } from "../../../framework/creator/CreatorCharacter/types"
 
 export class Jump extends Skill {
+  private _distance: number
+  private _power: number
+  private _distanceCurrent: number
+  public active: boolean
+
   public constructor(
     character: ICreatorCharacter,
     cb: () => void,
     destroy: () => void,
   ) {
     super(character, cb, destroy)
+
+    this._distance = 0
+    this._power = 0
+    this._distanceCurrent = 0
+    this.active = false
   }
 
-  update(): void {}
-
-  make(power: number = 200): void {
-    if (this._character.y === 0) {
+  update(): void {
+    if (this._distance > this._distanceCurrent) {
       this._cb()
-      this._character.y = power
+      this._distanceCurrent += this._power
+      this._character.y = this._power
+    }
+
+    if (this._distanceCurrent >= this._distance) {
+      this._distance = 0
+      this.active = false
     }
   }
 
-  destroy() {
+  make(distance: number = 200, power: number = 20): void {
+    if (this._character.y === 0) {
+      this.active = true
+      this._distance = distance
+      this._power = power
+      this._distanceCurrent = 0
+    }
+  }
+
+  destroy(): void {
     this._destroy()
   }
 }
