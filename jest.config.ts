755a8dfe738c6cf8f18dd0616ef5633a5d0e887b
GurovDmitriy <gurovdmitriy1991@gmail.com>
2023-10-23 11:29:58 +0300

    feat: todo test

--- jest.config.ts
+++ jest.config.ts
@@ -19,10 +19,10 @@ const config: Config = {
   clearMocks: true,
 
   // Indicates whether the coverage information should be collected while executing the test
-  // collectCoverage: false,
+  // collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  // collectCoverageFrom: ["<rootDir>/src/utils/**/*.ts"],
 
   // The directory where Jest should output its coverage files
   // coverageDirectory: undefined,
---
+++ src/game/framework/util/__tests__/ObservableCreator.test.ts
@@ -0,0 +1,4 @@
+describe("Utils Framework: ObservableCreator", () => {
+  it.todo("should be subscribe object")
+  it.todo("should be returns unsubscribe function")
+})
