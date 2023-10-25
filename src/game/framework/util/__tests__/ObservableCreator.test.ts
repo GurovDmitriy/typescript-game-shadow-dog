8402e4e643860dd37ac3f23ec245171b6ee3beee
GurovDmitriy <gurovdmitriy1991@gmail.com>
2023-10-25 10:30:48 +0300

    feat: test observable creator

--- src/game/framework/util/__tests__/ObservableCreator.test.ts
+++ src/game/framework/util/__tests__/ObservableCreator.test.ts
@@ -1,4 +1,68 @@
+import { ObservableCreator } from "../ObservableCreator/ObservableCreator"
+import { IObservableCreator } from "../ObservableCreator/types"
+
 describe("Utils Framework: ObservableCreator", () => {
-  it.todo("should be subscribe object")
-  it.todo("should be returns unsubscribe function")
+  type TSubscriber = { name: string; update?: () => void }
+  let subscriber: TSubscriber
+  let observableCreator: IObservableCreator<TSubscriber, undefined>
+
+  beforeEach(() => {
+    subscriber = { name: "John" }
+    observableCreator = new ObservableCreator<TSubscriber, undefined>()
+  })
+
+  test("should be add subscribe in list", () => {
+    // Arrange
+    const sut = observableCreator
+    const subscribers = sut.subscribers
+
+    // Act
+    sut.subscribe(subscriber)
+
+    // Assert
+    expect(subscribers).toContain(subscriber)
+    expect(subscribers.size).toBe(1)
+  })
+
+  test("should be available unsubscribe with value", () => {
+    // Arrange
+    const sut = observableCreator
+    const subscribers = sut.subscribers
+    sut.subscribe(subscriber)
+
+    // Act
+    sut.unsubscribe(subscriber)
+
+    // Assert
+    expect(subscribers.size).toBe(0)
+  })
+
+  test("should be available unsubscribe with returns function", () => {
+    // Arrange
+    const sut = observableCreator
+    const subscribers = sut.subscribers
+    const unsubscribe: () => void = sut.subscribe(subscriber)
+
+    // Act
+    unsubscribe()
+
+    // Assert
+    expect(subscribers.size).toBe(0)
+  })
+
+  test("should be call subscriber with data", () => {
+    // Arrange
+    const update = jest.fn()
+    const data = { data: "data" }
+    const subscriber = { name: "John", update: update }
+    const sut = new ObservableCreator<typeof subscriber, typeof data>()
+    sut.subscribe(subscriber)
+
+    // Act
+    sut.notify(data)
+
+    // Assert
+    expect(update).toBeCalledWith(data)
+    expect(update).toBeCalledTimes(1)
+  })
 })
